37dcfd47aad1d764758162b66cc0ed34bc8a545c
RBletso <138777363+RBletso@users.noreply.github.com>
2023-07-12 15:59:50 +0100

    King

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -8,4 +8,51 @@ export default class King extends Piece {
     getAvailableMoves(board) {
         return new Array(0);
     }
-}
+    getAvailableMoves(board) {
+        let location = board.findPiece(this)
+        let moves = []
+        let row = location.row
+        let col = location.col
+
+
+        const directions = [
+          {row:1, col:0},
+          {row:0, col:1},
+          {row:-1, col:0},
+          {row:0, col:-1},
+          {row:1, col:1},
+          {row:-1, col:1},
+          {row:-1, col:-1},
+          {row:1, col:-1},
+        ]
+
+        for ( let direction of directions) {
+                const testSquare = Square.at(row + direction.row * i, col + direction.col * i);
+
+                // Is square on the board
+
+                if ( testSquare.row > 7 || testSquare.row < 0 || testSquare.col > 7 || testSquare.col < 0) {
+                    break;
+                }
+
+                const piece = board.getPiece(testSquare);
+
+                if ( piece === undefined) {
+                    moves.push(testSquare);
+                    continue;
+                }
+
+                if ( piece.player !== this.player ) {
+                    moves.push(testSquare);
+                }
+                break;
+            }
+
+        
+        
+
+
+        return moves ;
+        }
+    }
+
--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -1,5 +1,31 @@
 import King from '../../../src/engine/pieces/king';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
+import 'chai/register-should';
 
 describe('King', () => {
+    let board;
+    beforeEach(() => board = new Board());
 
+    it('can move laterally', () => {
+    const king = new King(Player.WHITE);
+    board.setPiece(Square.at(1, 2), king);
+
+    const moves = king.getAvailableMoves(board);
+
+    const expectedMoves = [
+        // Horizontal
+        Square.at(1, 1), Square.at(1, 3),
+        // Vertical
+        Square.at(0, 2), Square.at(2, 2),
+        // Diagonal +
+        Square.at(0, 1), Square.at(2, 3),
+        // Diagonal -
+        Square.at(0, 3), Square.at(2, 1),
+    ];
+
+    moves.should.deep.include.members(expectedMoves);
 });
+});
+
