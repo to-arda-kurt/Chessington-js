96fd6c51718f7bdd5325d0e2c4be051e007ded9c
Arda Kurt <to.arda.kurt@gmail.com>
2023-07-12 09:40:06 +0100

    Bishop moves

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
-import Piece from './piece';
+import Player from '..player';
+import Square from '..square';
+import Piece from '.piece';
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,59 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let moves = []
+
+        for( let i = 1; i=7; i++ ){
+            if(location.col + i  8 && location.row + i  8){
+                moves.push(Square.at(location.row+i,location.col+i))
+            }
+
+            if(location.col + i  8 && location.row - i = 0){
+                moves.push(Square.at(location.row-i,location.col+i))
+            }
+
+            if(location.col - i = 0 && location.row - i = 0){
+                moves.push(Square.at(location.row-i,location.col-i))
+            }
+
+            if(location.col - i = 0 && location.row + i  8){
+                moves.push(Square.at(location.row+i,location.col-i))
+            }
+        }
+
+
+
+
+        //   3,3 ++
+        //  for (let i = 1; i = 7; i++) {
+
+        //      moves.push(Square.at(location.row + i, location.col + i));
+
+        //  }
+
+        //   -+
+        //  for (let i = 1; i = 7; i++) {
+
+        //      moves.push(Square.at(location.row + i, location.col - i));
+
+        //  }
+
+        //   --
+        //  for (let i = 1; i = 7; i++) {
+
+        //      moves.push(Square.at(location.row - i, location.col - i));
+
+        //  }
+
+        //   +-
+        //  for (let i = 1; i = 7; i++) {
+
+        //      moves.push(Square.at(location.row - i, location.col + i));
+
+        //  }
+
+
+        return moves ;
     }
 }
